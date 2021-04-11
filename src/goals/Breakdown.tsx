10df54443d59b566695a0ff70166597dc8c1e364
Bartosz Krajka <bartosz.krajka@droidsonroids.pl>
2021-04-11 21:58:09 +0200

    Split breakdowns into something that can be turned into state

--- src/goals/Breakdown.tsx
+++ src/goals/Breakdown.tsx
@@ -18,8 +18,68 @@ const useStyles = makeStyles({
     minWidth: 650,
   },
 });
+
+type Level = 1 | 2 | 3;
+
+type Tile = {
+  level: Level;
+  value: number;
+};
+
+type Breakdown = {
+  name: string;
+  "2021": Tile | null;
+  "2022": Tile | null;
+  "2023": Tile | null;
+  "2024": Tile | null;
+  "2025": Tile | null;
+  "2026": Tile | null;
+};
+
+type Timeslot = keyof typeof Breakdown;
+
 export const Breakdown = ({ goals }: { goals: Goal[] }) => {
   const classes = useStyles();
+
+  const timeSlots = ["2021", "2022", "2023", "2024", "2025", "2026"];
+
+  const getInitialBreakdowns = (): Breakdown[] => {
+    return goals.map((goal: Goal) => ({
+      name: goal.name,
+      "2021": { level: 1, value: goal.level1 },
+      "2022": { level: 2, value: goal.level2 },
+      "2023": { level: 3, value: goal.level3 },
+      "2024": null,
+      "2025": null,
+      "2026": null,
+    }));
+  };
+
+  const initialBreakdowns: Breakdown[] = getInitialBreakdowns();
+
+  const renderTimeSlot = (
+    timeSlot: string,
+    breakdown: Breakdown,
+    index: number
+  ) => {
+    const x = timeSlot as Timeslot;
+    if (breakdown[x])
+      return (
+        <LevelTableCell level={(breakdown[x] as Tile).level} index={index}>
+          {(breakdown[x] as Tile).value}
+        </LevelTableCell>
+      );
+
+    return <EmptyTableCell index={index} time={timeSlot} />;
+  };
+
+  const renderGoalRow = (breakdown: Breakdown, index: number) => (
+    <TableRow key={breakdown.name}>
+      <TableCell align="right">{index + 1}</TableCell>
+      <TableCell align="right">{breakdown.name}</TableCell>
+      {timeSlots.map((timeslot) => renderTimeSlot(timeslot, breakdown, index))}
+    </TableRow>
+  );
 
   return (
     <DndProvider backend={HTML5Backend}>
@@ -29,36 +89,12 @@ export const Breakdown = ({ goals }: { goals: Goal[] }) => {
             <TableRow>
               <TableCell>#</TableCell>
               <TableCell align="right">Nazwa</TableCell>
-              <TableCell align="right">2021</TableCell>
-              <TableCell align="right">2022</TableCell>
-              <TableCell align="right">2023</TableCell>
-              <TableCell align="right">2024</TableCell>
-              <TableCell align="right">2025</TableCell>
-              <TableCell align="right">2026</TableCell>
+              {timeSlots.map((timeSlot) => (
+                <TableCell align="right">{timeSlot}</TableCell>
+              ))}
             </TableRow>
           </TableHead>
-          <TableBody>
-            {goals.map((goal: Goal, index: number) => (
-              <TableRow key={goal.name}>
-                <TableCell align="right">{index + 1}</TableCell>
-                <TableCell align="right">{goal.name}</TableCell>
-
-                <LevelTableCell level={1} index={index}>
-                  {goal.level1}
-                </LevelTableCell>
-                <LevelTableCell level={2} index={index}>
-                  {goal.level2}
-                </LevelTableCell>
-                <LevelTableCell level={3} index={index}>
-                  {goal.level3}
-                </LevelTableCell>
-
-                <EmptyTableCell index={index} time={"2024"} />
-                <EmptyTableCell index={index} time={"2025"} />
-                <EmptyTableCell index={index} time={"2026"} />
-              </TableRow>
-            ))}
-          </TableBody>
+          <TableBody>{initialBreakdowns.map(renderGoalRow)}</TableBody>
         </Table>
       </TableContainer>
     </DndProvider>
